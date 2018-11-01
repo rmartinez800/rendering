1c6dce93d7ab91111912d94ce921369d1ccc7d30
rmartinez800 <robertmartinez280@gmail.com>
2018-10-31 21:38:06 -0500

    5 done

--- renderNametags.js
+++ renderNametags.js
@@ -1,8 +1,17 @@
 
 function renderNametags(nametags) {
+    var nametagsHTML = nametags.map(function(currentName){
+        return ` 
+            <div class="nametag mx-auto w-50 text-center border m-1">
+                <div class="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
+                <div class="name h4 p-5">${currentName}</div>
+            </div>
+        `;
+    });   
+
     return `
-        <div class="text-center mt-5">
-        <code>${JSON.stringify(nametags)}</code>
+        <div class="nametagContainer">
+            ${nametagsHTML.join('')}           
         </div>
     `
 }
@@ -11,6 +20,7 @@ function nametags() {
     var content = document.getElementById('content');
 
     var nametagsAbstraction = [
+
        "Kamilah",
        "Kim",
        "Stuart",
